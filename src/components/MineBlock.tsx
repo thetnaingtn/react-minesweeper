e343a28342ac805e3c5c0560dda92f6917eb4f21
Thet Naing Tun <thetnaingtun@bindez.com>
2022-04-14 18:12:38 +0630

    modify mine block style

--- src/components/MineBlock.tsx
+++ src/components/MineBlock.tsx
@@ -1,4 +1,5 @@
 import IconMine from '~icons/mdi/mine';
+import IconFlag from '~icons/mdi/flag';
 import { BlockState } from '../types';
 
 type MineBlockProps = {
@@ -7,8 +8,13 @@ type MineBlockProps = {
 
 export default function MineBlock({ block }: MineBlockProps) {
   return (
-    <button className="flex items-center justify-center min-w-8 min-h-8">
-      <IconMine />
+    <button className="flex items-center justify-center min-w-8 min-h-8 border-0.5 border-gray-400 bg-gray-500 hover:bg-gray-400">
+      {block.flaged && (
+        <span className="text-red-400">
+          <IconFlag />
+        </span>
+      )}
+      {block.revealed && (block.mine ? <IconMine /> : block.adjacentMines)}
     </button>
   );
 }
