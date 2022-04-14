ac662b8c7b0551961bc6565c63f98a22c12b5250
Thet Naing Tun <thetnaingtun@bindez.com>
2022-04-14 11:00:08 +0630

    extend min width utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      minWidth: {
+        8: '2rem',
+      },
+      minHeight: {
+        8: '2rem',
+      },
+    },
   },
   plugins: [],
-}
+};
